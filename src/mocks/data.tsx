a28f94cee4af6f1b13ec20204917945456f06181
Edyta SKIBINSKA <edyta.skibinska@upsideo.net>
2020-05-25 14:49:16 +0200

    ignoring deistribution files

---
+++ src/mocks/data.tsx
@@ -0,0 +1,33 @@
+interface dataInterface {
+    products: {
+        id: number;
+        title: string;
+        description: string;
+        date: string;
+    }[];
+}
+
+const data: dataInterface = {
+    products: [
+        {
+            id: 1,
+            title: "Card 1",
+            description: "Lorem ipsum dolor sit amet ..",
+            date : "12/02/2020"
+        },
+        {
+            id: 2,
+            title: "Card 2",
+            description: "Lorem ipsum dolor sit amet ..",
+            date : "17/05/2020"
+        },
+        {
+            id: 3,
+            title: "Card 3",
+            description: "Lorem ipsum dolor sit amet ..",
+            date : "10/06/2020"
+        }
+    ]
+}
+
+export default data
