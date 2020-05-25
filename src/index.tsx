c50df14a476dbaf501268ff29b1d04bfffe32f4b
Edyta SKIBINSKA <edyta.skibinska@upsideo.net>
2020-05-25 09:42:41 +0200

    setting up application

---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import * as React from "react"
+import * as ReactDOM from "react-dom"
+
+const ROOT= document.querySelector(".container");
+
+ReactDOM.render(
+    <h1>Hello world</h1>,
+   ROOT
+  );
