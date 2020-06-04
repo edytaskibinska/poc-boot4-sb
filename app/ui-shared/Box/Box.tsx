50ed237c10ddf53aaa7cd335b97d3136f9608d23
Edyta SKIBINSKA <edyta.skibinska@upsideo.net>
2020-06-04 16:05:21 +0200

    happy box documentation test

--- app/ui-shared/Box/Box.tsx
+++ app/ui-shared/Box/Box.tsx
@@ -1,3 +1,17 @@
+/** @flow */
+
+/**
+ * Computes the difference between two array references. 
+ * @name Box 
+ * @example
+ * import React from 'react';
+ * import Box from '@bit/upsideodev-ui.juliette-business.box';
+ *
+ * export default (
+ *	<Box color="red">Happy box</Box>
+ *)
+ */
+
 import React, { FC, ReactNode } from 'react'
 
 import { BoxStyled } from "./BoxStyle"
